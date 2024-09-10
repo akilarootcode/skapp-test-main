556ccd0d72fef428758b138b508a49d8f9269717
Akila Sachintha <akila.silva@rootcode.io>
2024-09-10 09:36:06 +0530

    added project

---
+++ src/main/java/com/skapp/SkappApplication.java
@@ -0,0 +1,13 @@
+package com.skapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkappApplication.class, args);
+	}
+
+}
